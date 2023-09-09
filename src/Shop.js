9fc0b54f77e3ed329877dc1c2fdc7ed447c7ea3c
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-09-09 17:25:34 +0530

    state define

--- src/Shop.js
+++ src/Shop.js
@@ -4,13 +4,13 @@ function Shop() {
     return (
         <div className='container text-center my-5'>
 
-            <h3 className='my-4'>Adidas Shoes 50Rs.</h3>
+            <h3 className='my-4'>Deposit/Withdraw Money</h3>
 
             <button className="btn btn-primary mx-3"> <strong> - </strong> </button>
-            Add To Cart
+            Update Balance
             <button className="btn btn-primary mx-3"> <strong> + </strong></button>
         </div>
     )
 }
 
-export default Shop
+export default Shop;
