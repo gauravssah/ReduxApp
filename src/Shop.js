9669e7fbaa423ecd5f8ed642d9e6544c31842725
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-09-09 18:42:47 +0530

    updationg using dispatch

--- src/Shop.js
+++ src/Shop.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { bindActionCreaters } from 'redux';
+import { actionCreators } from './state/index';
+
+
 
 function Shop() {
+    const dispatch = useDispatch();
+
     return (
         <div className='container text-center my-5'>
             <h3 className='my-4'>Deposit/Withdraw Money</h3>
-            <button className="btn btn-primary mx-3"> <strong> - </strong> </button>
+            <button className="btn btn-primary mx-3" onClick={() => { dispatch(actionCreators.withdrawMoney(100)) }} > <strong> - </strong> </button>
             Update Balance
-            <button className="btn btn-primary mx-3"> <strong> + </strong></button>
+            <button className="btn btn-primary mx-3" onClick={() => { dispatch(actionCreators.depositMoney(100)) }} > <strong> + </strong></button>
         </div>
     )
 }
