59573a7eaebe65f0673b2fa19c33b9008e6c99f2
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-09-09 14:53:14 +0530

    files added

---
+++ src/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Shop() {
+    return (
+        <div className='container text-center my-5'>
+
+            <h3 className='my-4'>Adidas Shoes 50Rs.</h3>
+
+            <button className="btn btn-primary mx-3"> <strong> - </strong> </button>
+            Add To Cart
+            <button className="btn btn-primary mx-3"> <strong> + </strong></button>
+        </div>
+    )
+}
+
+export default Shop
