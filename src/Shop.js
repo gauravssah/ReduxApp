7ddeb0b3d9c440237c0c73f1d1d21d6f5c43493d
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-09-09 18:31:45 +0530

    using useSelector

--- src/Shop.js
+++ src/Shop.js
@@ -3,9 +3,7 @@ import React from 'react'
 function Shop() {
     return (
         <div className='container text-center my-5'>
-
             <h3 className='my-4'>Deposit/Withdraw Money</h3>
-
             <button className="btn btn-primary mx-3"> <strong> - </strong> </button>
             Update Balance
             <button className="btn btn-primary mx-3"> <strong> + </strong></button>
