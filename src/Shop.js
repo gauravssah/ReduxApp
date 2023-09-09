21cb62a3315f81de0af1ea8babe9baed8530aa09
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-09-09 18:55:56 +0530

    updating using bindactioncreaters

--- src/Shop.js
+++ src/Shop.js
@@ -1,19 +1,29 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
-import { bindActionCreaters } from 'redux';
+import { bindActionCreators } from 'redux';
 import { actionCreators } from './state/index';
 
 
 
 function Shop() {
     const dispatch = useDispatch();
+    // const actions = bindActionCreaters(actionCreators.dispatch);
+    const { withdrawMoney, depositMoney } = bindActionCreators(actionCreators, dispatch)
 
     return (
         <div className='container text-center my-5'>
             <h3 className='my-4'>Deposit/Withdraw Money</h3>
-            <button className="btn btn-primary mx-3" onClick={() => { dispatch(actionCreators.withdrawMoney(100)) }} > <strong> - </strong> </button>
+
+            {/* <button className="btn btn-primary mx-3" onClick={() => { dispatch(actionCreators.withdrawMoney(100)) }} > <strong> - </strong> </button>
             Update Balance
-            <button className="btn btn-primary mx-3" onClick={() => { dispatch(actionCreators.depositMoney(100)) }} > <strong> + </strong></button>
+            <button className="btn btn-primary mx-3" onClick={() => { dispatch(actionCreators.depositMoney(100)) }} > <strong> + </strong></button> */}
+
+            <button className="btn btn-primary mx-3" onClick={() => { withdrawMoney(100) }} > <strong> - </strong> </button>
+            Update Balance
+            <button className="btn btn-primary mx-3" onClick={() => { depositMoney(100) }} > <strong> + </strong></button>
+
+
+
         </div>
     )
 }
